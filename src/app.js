739072532972f96a5ff6369df36a61896c01740b
SultanaK <kr.shampa@yahoo.com>
2020-10-29 21:17:25 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(helmet())
 app.use(cors())
 
 app.get('/', (req, res) => {
-   res.send('Hello, boilerplate!!')
+   res.send('Hello, from AnimationStation!!')
  })
 app.use(function errorHandler(error, req, res, next) {
    
