6b9dfdb7c3dfe30892d64c3fa857796d1f8bbdc9
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-11-15 15:13:43 -0700

    change DB_URL to DATABASE_URL

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/animation-station',
-  TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://postgres@localhost/animation-station-test',
+  DATABASE_URL: process.env.DB_URL || 'postgresql://postgres@localhost/animation-station',
+  TEST_DATABASE_URL: process.env.TEST_DB_URL || 'postgresql://postgres@localhost/animation-station-test',
   JWT_SECRET: process.env.JWT_SECRET || 'use-env-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '60s',
 }
--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 const app = require('./app')
 require('dotenv').config()
 const knex = require('knex')
-const { PORT, DB_URL } = require('./config')
+const { PORT, DATABASE_URL } = require('./config')
 
 
 
 const db = knex({
   client: 'pg',
-  connection: DB_URL,
+  connection: DATABASE_URL,
 })
 
 app.set('db', db)
