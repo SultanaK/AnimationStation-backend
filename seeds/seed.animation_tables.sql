0a5799c141ff7d273a80a895a82c95c7b7a0c708
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-11-15 20:17:53 -0700

    fix seed data

--- seeds/seed.animation_tables.sql
+++ seeds/seed.animation_tables.sql
@@ -10,7 +10,7 @@ VALUES
   ('dunder', 'Dunder Mifflin', 'dunder@email.com', '$2a$10$jOUf1PzodRaZm1gWz3w/K.QQoB9.giNHzPQ5v65EUsWhn4545kisy'),
   ('b.deboop', 'Bodeep Deboop', 'Bo@gmail.com', '$2a$10$SqPusKk3PDk.GvBIfyitqORO5bujAXMOKN7SUryMadFTCcT0XTZ8m'),
   ('wippy', 'Ping Won In', 'Ping@yahoo.com', '$2a$10$GGBXtEu3Fglwd/Pawi5F5.A/NObHkfnf0JmZiNHaXypeUd6A6UpUe'),
-  ('Ping', 'Ping Won In', 'won@yahoo.com', '$2a$10$qh7H8r659496l4ISPNujquOGSkbHWsjLo9mTmRa9rjcraTrEdnPzS');
+  ('Ping', 'Ping Won In', 'won@yahoo.com', '$2a$10$qh7H8r659496l4ISPNujquOGSkbHWsjLo9mTmRa9rjcraTrEdnPzS'),
   ('nathanielmata', 'Nathaniel Mata', 'nate@nathanielmata.com', '$2a$12$m/8jl1QtXbncFH3D5gdBM.XDeGQu.dDgfl80hUxj0aZBCxkxtxoDK');
 
 
