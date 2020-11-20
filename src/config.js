27dbd7bfbb220490a6c9d842368501e4f92669e8
SultanaK <kr.shampa@yahoo.com>
2020-11-19 22:04:57 -0600

    add expiry time

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ module.exports = {
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/animation-station',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/animation-station-test',
   JWT_SECRET: process.env.JWT_SECRET || 'use-env-secret',
-	JWT_EXPIRY: process.env.JWT_EXPIRY || '460s'
+	JWT_EXPIRY: process.env.JWT_EXPIRY || '4600s'
 }
 
