f09e3747f890ea3bbf42c4443902b7b90ae6dee1
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-11-20 23:38:50 -0700

    fix iteration value not saving on update

--- src/endpoints/animation/animations-router.js
+++ src/endpoints/animation/animations-router.js
@@ -7,89 +7,96 @@ const animationsRouter = express.Router();
 const jsonBodyParser = express.json();
 
 animationsRouter
-    .route("/")
-    .all(requireAuth)
-    .get((req, res, next) => {
-        AnimationsService.getAllAnimations(req.app.get("db"), req.user.id)
-            .then((animations) => {
-                res.json(AnimationsService.serializeAnimations(animations));
-            })
-            .catch(next);
-    })
-    .post(jsonBodyParser, (req, res, next) => {
-        const { title, delay, duration, direction, iteration, timing, fill, keyframe, target } = req.body;
-        const newAnimation = { title, delay, duration, direction, iteration, timing, fill, keyframe, target }
+	.route('/')
+	.all(requireAuth)
+	.get((req, res, next) => {
+		AnimationsService.getAllAnimations(
+      req.app.get('db'),
+      req.user.id
+      )
+			.then((animations) => {
+				res.json(AnimationsService.serializeAnimations(animations));
+			})
+			.catch(next);
+	})
+	.post(jsonBodyParser, (req, res, next) => {
+		const { title, delay, duration, direction, iteration, timing, fill, keyframe, target } = req.body;
+		const newAnimation = { title, delay, duration, direction, iteration, timing, fill, keyframe, target };
 
-        for (const [key, value] of Object.entries(newAnimation)) {
-            if (value == null || value === "") {
-                return res.status(400).json({
-                    error: `Missing '${key}' in request body`
-                })
-            }
-        }
+		for (const [key, value] of Object.entries(newAnimation)) {
+			if (value == null || value === '') {
+				return res.status(400).json({
+					error: `Missing '${key}' in request body`,
+				});
+			}
+		}
 
-        newAnimation.user_id = req.user.id
-
-        AnimationsService.insertAnimation(
-            req.app.get('db'),
-					newAnimation,
-						req.user.id,
-        )
-            .then(animation => {
-                res
-                    .status(201)
-                    .location(path.posix.join(req.originalUrl, `/${animation.id}`))
-                    .json(AnimationsService.serializeAnimation(animation))
-            })
-            .catch(next)
-    });
+    newAnimation.user_id = req.user.id;
+    
+		AnimationsService.insertAnimation(req.app.get('db'), newAnimation, req.user.id)
+			.then((animation) => {
+				res
+					.status(201)
+					.location(path.posix.join(req.originalUrl, `/${animation.id}`))
+					.json(AnimationsService.serializeAnimation(animation));
+			})
+			.catch(next);
+	});
 
 animationsRouter
-  .route('/:id')
-  .all(requireAuth)
-  .all(checkAnimationExists)
-  .get((req, res) => {
-      res.json(AnimationsService.serializeAnimation(res.animation));
-  })
-  .patch(jsonBodyParser, (req, res, next) => {
-        const { title, delay, duration, direction, timing, fill, keyframe, target } = req.body;
-        const animationToUpdate = { title, delay, duration, direction, timing, fill, keyframe, target };
-        for (const [key, value] of Object.entries(animationToUpdate)) {
-            if (value == null || value === "") {
-                return res.status(400).json({
-                    error: `Missing '${key}' in request body`
-                })
-            }
-        }
-        AnimationsService.updateAnimation(
-            req.app.get("db"),
-            req.params.id,
-            req.user.id,
-            animationToUpdate
-        )
-        .then(animation => {
-            res
-                .status(201)
-                .location(path.posix.join(req.originalUrl, `/${animation.id}`))
-                .json(AnimationsService.serializeAnimation(animation))
-        })
-        .catch(next)
-    })
-
-
-	.delete ((req, res, next) => {
+	.route('/:id')
+	.all(requireAuth)
+	.all(checkAnimationExists)
+	.get((req, res) => {
+		res.json(AnimationsService.serializeAnimation(res.animation));
+	})
+	.patch(jsonBodyParser, (req, res, next) => {
+		const an = req.body;
+		const animationToUpdate = { 
+      title: an.title,
+      delay: an.delay,
+      duration: an.duration,
+      iteration: an.iteration,
+      direction: an.direction,
+      timing: an.timing,
+      fill: an.fill,
+      keyframe: an.keyframe,
+      target: an.target,
+    };
+    
+    for (const [key, value] of Object.entries(animationToUpdate)) {
+			if (value == null || value === '') {
+				return res.status(400).json({
+					error: `Missing '${key}' in request body`,
+				});
+			}
+		}
+    
+    AnimationsService.updateAnimation(
+      req.app.get('db'),
+      req.params.id,
+      req.user.id,
+      animationToUpdate,
+      )
+			.then((animation) => {
+				res
+					.status(201)
+					.location(path.posix.join(req.originalUrl, `/${animation.id}`))
+					.json(AnimationsService.serializeAnimation(animation));
+			})
+			.catch(next);
+	})
+	.delete((req, res, next) => {
 		AnimationsService.deleteAnimation(
-			req.app.get('db'),
-			req.params.id,
-			req.user.id
-		)
+      req.app.get('db'),
+      req.params.id,
+      req.user.id,
+      )
 			.then((numRowsAffected) => {
 				if (numRowsAffected === 1) {
 					res.status(204).end();
 				} else {
-					res.status(401)
-						.json({error: `You are not authorize to delete thia animation`})
-						
+					res.status(401).json({ error: `You are not authorize to delete thia animation` });
 				}
 				console.log(numRowsAffected);
 			})
